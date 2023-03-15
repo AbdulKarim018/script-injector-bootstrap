8fd803a7f50aff38b4216f37ea0d9434070ad4bd
AbdulKarim018 <lackhorse.ak@gmail.com>
2023-03-16 00:23:35 +0500

    Only Final Steps Left.

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
-// let scriptInjected;
-// const src = "\scriptToInject.js"
-// const scriptLoader = () => {
-//     let script = document.createElement("script")
-//     script.src = src
-//     script.onerror = document.getElementById("scriptStatus").innerHTML = `Error fetching script from the SOURCE: "${src}"`
-//     document.body.appendChild(script)
-// }
-
-
-// btn = document.getElementById("btn")
-// btn.onclick = scriptStatus_show = () => {
-//     if (scriptInjected != true) {
-//         document.getElementById("scriptStatus").innerHTML = "Fetching Script..."
-//         setTimeout(function () { document.getElementById("scriptStatus").innerHTML = "Verifying Script..." }, 2000)
-//         setTimeout(function () { document.getElementById("scriptStatus").innerHTML = "Loading Script..." }, 3000)
-//         setTimeout(function () { document.getElementById("scriptStatus").innerHTML = "Injecting Script..." }, 5000)
-//         setTimeout(scriptLoader, 7000)
-//         scriptInjected = true
-//     } else if (scriptInjected == true) {
-//         document.getElementById("scriptStatus").innerHTML = "Script is already Injected.<br>If the script doesnot work try reloading the page or provide a valid source."
-//     }
-// }
-
-//     NEW CODE 
-
 let scriptInjected;
+const spinner = document.getElementsByClassName("spinner-border")[0]
+spinner.setAttribute("hidden", true)
+const spinner_show = () => {
+    spinner.removeAttribute("hidden")
+}
+const spinner_hide = () => {
+    spinner.setAttribute("hidden", true)
+}
+const check_icon = `<i class="fa-solid fa-check"></i>`
+const scriptStatus = document.getElementById("scriptStatus")
 const src = "\scriptToInject.js"
 const scriptLoader = () => {
     let script = document.createElement("script")
     script.src = src
-    script.onerror = document.getElementById("scriptStatus").innerHTML = `Error fetching script from the SOURCE: "${src}"`
+    script.onerror = scriptStatus.innerHTML = `Error fetching script from the SOURCE: "${src}"`
     document.body.appendChild(script)
 }
 
 btn = document.getElementById("btn")
 btn.onclick = scriptStatus_show = () => {
     if (scriptInjected != true) {
-        document.getElementById("scriptStatus").innerHTML = "Fetching Script..."
-        setTimeout(function () { document.getElementById("scriptStatus").innerHTML = "Verifying Script..." }, 2000)
-        setTimeout(function () { document.getElementById("scriptStatus").innerHTML = "Loading Script..." }, 3000)
-        setTimeout(function () { document.getElementById("scriptStatus").innerHTML = "Injecting Script..." }, 5000)
-        setTimeout(scriptLoader, 7000)
+        scriptStatus.innerHTML = "Fetching Script..."
+        spinner_show()
+        setTimeout(() => {
+            scriptStatus.innerHTML = `Script Fetched ${check_icon}`
+            spinner_hide()
+        }, 2000)
+        setTimeout(() => {
+            scriptStatus.innerHTML = "Verifying Script..."
+            spinner_show()
+            setTimeout(() => {
+                scriptStatus.innerHTML = `Script Verified ${check_icon}`
+                spinner_hide()
+            }, 1000)
+        }, 3000)
+        setTimeout(() => {
+            scriptStatus.innerHTML = "Injecting Script..."
+            spinner_show()
+            setTimeout(() => {
+                scriptStatus.innerHTML = `Script Injected ${check_icon}`
+                spinner_hide()
+                scriptLoader()
+            }, 3000);
+        }, 5000)
         scriptInjected = true
     } else if (scriptInjected == true) {
+        spinner.setAttribute("hidden", true)
         document.getElementById("scriptStatus").innerHTML = "Script is already Injected.<br>If the script doesnot work try reloading the page or provide a valid source."
     }
 }
